d88f8c0eecf17d369906a3c504f2f964cc1baa88
Ryan Short <ryanshort.webdev@gmail.com>
2022-09-24 23:34:31 -0400

    added 'back to top' button

--- main/main.js
+++ main/main.js
@@ -7,6 +7,7 @@ const SEARCHAPI =
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const mybutton = document.getElementById("myBtn");
 
 getMovies(APIURL);
 
@@ -62,3 +63,19 @@ form.addEventListener("submit", (e) => {
         search.value = "";
     }
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+} 
